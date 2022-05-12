1910c38c852d0e643acd62ace7dd7c2f3b0d5b23
EugeBr <eugebordenave@gmail.com>
2022-05-12 14:50:23 +0200

    navbar

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ app.use(express.static("public"));
 hbs.registerPartials(__dirname + "/views/partials");
 
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true })); //*MIDDLEWARE
+//!this is needed when we use 'post' mothods
+//*is used between the request and the respons
+//*to access the body inside the request
 
 const index = require('./routes/index.routes');
 app.use('/', index);
